38de51bde91f7c2326135f0045f797ad0f3159ea
ajay-pratap-ai <ajaypratapsingh7600@gmail.com>
2026-06-12 23:26:17 +0530

    Solved LeetCode #79 Word Search using DFS and Backtracking

---
+++ WordSearchInAnArray.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    bool dfs(vector<vector<char>>& board, string& word,
+             int row, int col, int idx) {
+
+        if (idx == word.size()) return true;
+
+        int m = board.size();
+        int n = board[0].size();
+
+        if (row < 0 || col < 0 || row >= m || col >= n ||
+            board[row][col] != word[idx]) {
+            return false;
+        }
+
+        char temp = board[row][col];
+        board[row][col] = '#'; // mark visited
+
+        bool found =
+            dfs(board, word, row + 1, col, idx + 1) ||
+            dfs(board, word, row - 1, col, idx + 1) ||
+            dfs(board, word, row, col + 1, idx + 1) ||
+            dfs(board, word, row, col - 1, idx + 1);
+
+        board[row][col] = temp; // backtrack
+
+        return found;
+    }
+};
