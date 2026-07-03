a44ed13f118f02bacdd20c0f5f16cd255bb979d8
ajay-pratap-ai <ajaypratapsingh7600@gmail.com>
2026-07-03 23:22:12 +0530

    Solved LeetCode #120 Triangle using Dynamic Programming (Memoization)

---
+++ Triangle.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int solve(int i, int j, vector<vector<int>>& triangle,
+              vector<vector<int>>& dp) {
+
+        if (i == triangle.size() - 1)
+            return triangle[i][j];
+
+        if (dp[i][j] != INT_MAX)
+            return dp[i][j];
+
+        int down = solve(i + 1, j, triangle, dp);
+        int diagonal = solve(i + 1, j + 1, triangle, dp);
+
+        return dp[i][j] = triangle[i][j] + min(down, diagonal);
+    }
+
+    int minimumTotal(vector<vector<int>>& triangle) {
+
+        int n = triangle.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
+
+        return solve(0, 0, triangle, dp);
+    }
+};
