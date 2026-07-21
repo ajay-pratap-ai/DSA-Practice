e296725544b1dfb0d3bc775615c4c15920eee275
ajay-pratap-ai <ajaypratapsingh7600@gmail.com>
2026-07-21 22:19:10 +0530

    Day 51: Solved LeetCode 51 - N-Queens using Backtracking

---
+++ NQueens.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool IsSafe(vector<string>& board, int row, int col) {
+        int n = board.size();
+
+        // Check vertical
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q')
+                return false;
+        }
+
+        // Check left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        // Check right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        return true;
+    }
+
+    void NQueens(vector<string>& board, int row,
+                 vector<vector<string>>& ans) {
+        int n = board.size();
+
+        if (row == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (IsSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                NQueens(board, row + 1, ans);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n, string(n, '.'));
+
+        NQueens(board, 0, ans);
+
+        return ans;
+    }
+};
